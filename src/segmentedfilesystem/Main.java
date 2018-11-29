e9f8d37383978773d7d671c0f84904c31e3cf322
jubairmr9 <35503797+jubairmr9@users.noreply.github.com>
2018-11-29 09:05:00 -0600

    Start adding methods to Main.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -4,6 +4,21 @@ public class Main {
     
     public static void main(String[] args) {
         
+        int port;
+        InetAddress address = null;
+        DatagramSocket socket = null;
+        DatagramPacket packet;
+        byte[] sendBuf = new byte[256];
+        
+        
+        // Process the command-line args used to invoke the method
+        if (args.length != 1) {
+           System.out.println("Usage: java QuoteClient <hostname>");
+            return;
+        }
+        
+        
+        
     }
 
 }
