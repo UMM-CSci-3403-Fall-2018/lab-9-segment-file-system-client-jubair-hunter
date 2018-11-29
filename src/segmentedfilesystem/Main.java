f253a1073d4a233112fcd6851d02fbe49eb273e3
welch298 <welch298@morris.umn.edu>
2018-11-29 10:19:10 -0600

    Allow 'QuoteClient' send a request to the server and get a response

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,24 +1,34 @@
 package segmentedfilesystem;
 
 public class Main {
-    
+
     public static void main(String[] args) {
-        
+
+        //Declare local variables
         int port;
         InetAddress address = null;
         DatagramSocket socket = null;
         DatagramPacket packet;
         byte[] sendBuf = new byte[256];
-        
-        
+
+
         // Process the command-line args used to invoke the method
         if (args.length != 1) {
            System.out.println("Usage: java QuoteClient <hostname>");
             return;
-        }
-        
-        
-        
+          }
+
+        //QuoteClient sends a request to the server
+        DatagramSocket socket = new DatagramSocket();
+        byte[] buf = new byte[256];
+        InetAddress address = InetAddress.getByName(args[0]);
+        DatagramPacket packet = new DatagramPacket(buf, vuf.length, adress, 4445);
+        socket.send(packet);
+
+        //Client gets a response from the server and displays it
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+        String received - new String(packet.getData(), 0, packet.getLength());
     }
 
 }
