0e15646c7d9b68a211d0f131f8b542722177aa51
welch298 <welch298@morris.umn.edu>
2018-12-04 10:47:33 -0600

    Add port 6014

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -6,29 +6,28 @@ public class Main {
 
         //Declare local variables
         int port;
-        InetAddress address = null;
-        DatagramSocket socket = null;
+        InetAddress address = "heartofgold.morris.umn.edu";
+        DatagramSocket socket = 6014;
         DatagramPacket packet;
-        byte[] sendBuf = new byte[256];
+        byte[] sendBuf = new byte[1028];
 
 
         // Process the command-line args used to invoke the method
         if (args.length != 1) {
-           System.out.println("Usage: java QuoteClient <hostname>");
+           System.out.println("Usage: java QuoteClient heartofgold.morris.umn.edu");
             return;
           }
 
         //QuoteClient sends a request to the server
         DatagramSocket socket = new DatagramSocket();
-        byte[] buf = new byte[256];
-        InetAddress address = InetAddress.getByName(args[0]);
-        DatagramPacket packet = new DatagramPacket(buf, vuf.length, adress, 4445);
+        byte[] buf = new byte[1028];
+        InetAddress address = InetAddress.getByName(heartofgold.morris.umn.edu);
+        DatagramPacket packet = new DatagramPacket(buf, vuf.length, adress, 6014);
         socket.send(packet);
 
         //Client gets a response from the server and displays it
         packet = new DatagramPacket(buf, buf.length);
         socket.receive(packet);
-        String received - new String(packet.getData(), 0, packet.getLength());
-    }
+          }
 
 }
